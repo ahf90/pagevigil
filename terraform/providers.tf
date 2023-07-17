1f1e904fc5c83512fec7c53ae62a5304971017b5
Alex Friedman <ahfriedman09@gmail.com>
2023-07-16 21:15:50 -0400

    fix: terraform null_resource runs on version change (#31)

---
+++ terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    null = {
+      source  = "hashicorp/null"
+      version = "3.2.1"
+    }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.8.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.5.1"
+    }
+  }
+}
